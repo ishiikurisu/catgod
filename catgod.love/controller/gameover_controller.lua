0f6d71ac70a67ad4bdd62d3a020cf1af680300e3
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-07-13 13:19:11 -0300

    Scoreboard now implements reading and writting

--- catgod.love/controller/gameover_controller.lua
+++ catgod.love/controller/gameover_controller.lua
@@ -2,51 +2,51 @@ local gameover_view = require "view/gameover_view"
 local gameover_controller = { }
 
 gameover_controller.construct = function(score)
-    local self = { }
+  local self = { }
 
-    self.actions = { }
-    self.score = score
-    self.player = { 1, 1, 1 }
-    self.current = 1
-    self.view = gameover_view.new()
+  self.actions = { }
+  self.score = score
+  self.player = { 1, 1, 1 }
+  self.current = 1
+  self.view = gameover_view.new()
 
-    return self
+  return self
 end
 
 gameover_controller.new = function(score)
-    local self = gameover_controller.construct(score)
+  local self = gameover_controller.construct(score)
 
-    self.press = function(x, y)
-        local action = { x = x, y = y}
-        table.insert(self.actions, action)
+  self.press = function(x, y)
+    local action = { x = x, y = y}
+    table.insert(self.actions, action)
+  end
+
+  self.update = function(dt)
+    for _, action in pairs(self.actions) do
+      self.save()
+      return start.new()
     end
 
-    self.update = function(dt)
-        for _, action in pairs(self.actions) do
-            -- TODO Enable saving of stuff
-            return start.new()
-        end
-
-        self.actions = { }
-        return self
-    end
-
-    self.save = function()
-        local name = ""
-
-        for _, letter in pairs(self.player) do
-            name = name .. util.alphabet[letter]
-        end
-
-        util.store(name, self.score)
-    end
-
-    self.draw = function()
-        self.view.drawBasics(self.score)
-        self.view.drawPlayer(self.player, self.current)
-    end
-
+    self.actions = { }
     return self
+  end
+
+  self.save = function()
+    local name = ""
+
+    for _, letter in pairs(self.player) do
+      name = name .. util.alphabet[letter]
+    end
+
+    util.store(name, self.score)
+  end
+
+  self.draw = function()
+    self.view.drawBasics(self.score)
+    self.view.drawPlayer(self.player, self.current)
+  end
+
+  return self
 end
 
 return gameover_controller
--- catgod.love/controller/scoreboard_controller.lua
+++ catgod.love/controller/scoreboard_controller.lua
@@ -1,50 +1,54 @@
 local scoreboard_controller = { }
 
 scoreboard_controller.construct = function()
-    local self = { }
+  local self = { }
 
-    self.actions = { }
-    self.scoreboard = { }
+  self.actions = { }
+  self.scoreboard = { }
 
-    -- Loading scoreboard
-    local size, _ = love.filesystem.getSize(util.save_file)
-    -- TODO Check if save file exist
+  -- Loading scoreboard
+  local size, _ = love.filesystem.getSize(util.save_file)
+  if size ~= nil then
     local contents, size = love.filesystem.read(util.save_file, size)
     contents = util.splitstring(contents, "\n")
     for _, content in pairs(contents) do
-        local stuff = util.splitstring(content, " ")
-        table.insert(self.scoreboard, stuff)
+      local stuff = util.splitstring(content, " ")
+      table.insert(self.scoreboard, stuff)
     end
-
     table.sort(self.scoreboard, function(this, that) return tonumber(this[2]) > tonumber(that[2]) end)
+  end
 
-    return self
+  return self
 end
 
 scoreboard_controller.new = function()
-    local self = scoreboard_controller.construct()
+  local self = scoreboard_controller.construct()
 
-    self.press = function(x, y)
-        local action = { x = x, y = y }
-        table.insert(self.actions, action)
+  self.press = function(x, y)
+    local action = { x = x, y = y }
+    table.insert(self.actions, action)
+  end
+
+  self.update = function(dt)
+    for _, action in pairs(self.actions) do
+      return start.new()
     end
 
-    self.update = function(dt)
-        for _, action in pairs(self.actions) do
-            return start.new()
-        end
-
-        self.actions = { }
-        return self
-    end
-
-    self.draw = function()
-        for i, score in ipairs(self.scoreboard) do
-            love.graphics.print(i .. ". " .. score[1] .. " ... " .. score[2], 10, i*20)
-        end
-    end
-
+    self.actions = { }
     return self
+  end
+
+  self.draw = function()
+    if #self.scoreboard > 0 then
+      for i, score in ipairs(self.scoreboard) do
+        love.graphics.print(i .. ". " .. score[1] .. " ... " .. score[2], 50, i*50)
+      end
+    else
+      love.graphics.print("No one has played this yet! Why not be the first?", 50, 50)
+    end
+  end
+
+  return self
 end
 
 return scoreboard_controller
