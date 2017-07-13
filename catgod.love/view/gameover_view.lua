75cd3bd0ab4b61438b60870a48d539b7cf75d2ba
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-07-13 13:13:32 -0300

    Began work on gameover screen

--- catgod.love/view/gameover_view.lua
+++ catgod.love/view/gameover_view.lua
@@ -1,32 +1,53 @@
 local gameover_view = { }
 
+gameover_view.construct = function()
+  local self = { }
+
+  local x = love.graphics.getWidth()/4
+  local y = love.graphics.getHeight()/4
+  local firstUp = { x = x }
+  local firstDown = { }
+  local secondUp = { }
+  local secondDown = { }
+  local thirdUp = { }
+  local thirdDown = { }
+  self.buttons = { firstUp,
+                   firstDown,
+                   secondUp,
+                   secondDown,
+                   thirdUp,
+                   thirdDown }
+
+  return self
+end
+
 gameover_view.new = function()
-    local self = { }
+  local self = gameover_view.construct()
 
-    self.drawBasics = function(score)
-        local x = love.graphics.getWidth()/2
-        local y = love.graphics.getHeight()/2
+  self.drawBasics = function(score)
+    local x = love.graphics.getWidth()/4
+    local y = love.graphics.getHeight()/4
 
-        love.graphics.setColor(255, 0, 0)
-        love.graphics.print("GAME OVER", x, y)
-        love.graphics.print("SCORE: " .. score, x, y + 30)
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.print("GAME OVER", x, y)
+    love.graphics.print("SCORE: " .. score, x, y + 30)
+  end
+
+  self.drawPlayer = function(player, current)
+    local y = love.graphics.getHeight()/2 + 60
+    local x = love.graphics.getWidth()/2
+
+    for i, letter in ipairs(player) do
+      if i == current then
+        love.graphics.setColor(255, 229, 43)
+      else
+        love.graphics.setColor(255, 255, 255)
+      end
+      love.graphics.print(util.alphabet[player[i]], x + (i-1)*10, y)
     end
+  end
 
-    self.drawPlayer = function(player, current)
-        local y = love.graphics.getHeight()/2 + 60
-        local x = love.graphics.getWidth()/2
-
-        for i, letter in ipairs(player) do
-            if i == current then
-                love.graphics.setColor(255, 229, 43)
-            else
-                love.graphics.setColor(255, 255, 255)
-            end
-            love.graphics.print(util.alphabet[player[i]], x + (i-1)*10, y)
-        end
-    end
-
-    return self
+  return self
 end
 
 return gameover_view
